a851a12fdc1d019d10534f99d20f4abc3e9b8f54
Jeroen van Erp <jeroen@hierynomus.com>
2016-12-05 09:57:26 +0100

    Simplified day3

--- 2016-adventofcode.com/day3.py
+++ 2016-adventofcode.com/day3.py
@@ -1,6 +1,6 @@
 possible1 = 0
 possible2 = 0
-part2 = []
+triangles = []
 
 
 def possible(x, y, z):
@@ -9,16 +9,14 @@ def possible(x, y, z):
 
 
 with open('day3.in', 'r') as f:
-    for l in f:
-        arr = [int(c) for c in l.strip().split(' ') if c.strip()]
-        part2.append(arr)
-        if possible(*arr):
-            possible1 += 1
+    triangles = [[int(c) for c in l.strip().split(' ') if c.strip()] for l in f]
 
-for i in range(0, len(part2), 3):
+for i in range(0, len(triangles), 3):
     for j in range(0, 3):
-        if possible(part2[i][j], part2[i + 1][j], part2[i + 2][j]):
+        if possible(triangles[i][j], triangles[i + 1][j], triangles[i + 2][j]):
             possible2 += 1
+        if possible(*triangles[i + j]):
+            possible1 += 1
 
 print("Day 3.1: %s" % possible1)
 print("Day 3.1: %s" % possible2)
