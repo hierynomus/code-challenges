ef028a549963f5aed4fd683b9de2c2e8cdbbfd1e
Jeroen van Erp <jeroen@hierynomus.com>
2016-12-20 10:39:44 +0100

    Updated a bit

--- 2016-adventofcode.com/day20.py
+++ 2016-adventofcode.com/day20.py
@@ -1,30 +1,24 @@
-
-def solve(ranges):
-    lowest = None
-    curr = 0
-    count = 0
-    for low, high in ranges:
-        if curr < low:
-            count += low - curr
-            curr = high + 1
-            if not lowest:
-                lowest = curr
-        elif curr < high:
-            curr = high + 1
-
-    if curr < 4294967295:
-        count += 4294967295 - curr
-    return lowest, count
-
-
 ranges = []
 
 with open('day20.in', 'r') as f:
-    for line in f:
-        low, high = map(int, line.strip().split('-'))
-        ranges.append((low, high))
+    ranges = [tuple(map(int, line.strip().split('-'))) for line in f]
 
 ranges.sort()
-lowest, count = solve(ranges)
+lowest = None
+curr = 0
+count = 0
+
+for low, high in ranges:
+    if curr < low:
+        count += low - curr
+        curr = high + 1
+        if not lowest:
+            lowest = curr
+    elif curr < high:
+        curr = high + 1
+
+if curr < 4294967295:
+    count += 4294967295 - curr
+
 print("Day 20.1: %s" % lowest)
 print("Day 20.2: %s" % count)
