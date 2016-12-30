814ca0a3279b4f72518666256e39f5f26d965143
Jeroen van Erp <jeroen@hierynomus.com>
2016-12-30 15:31:15 +0100

    Significantly improved speed of day12

--- 2016-adventofcode.com/day12.py
+++ 2016-adventofcode.com/day12.py
@@ -1,31 +1,54 @@
-instructions = []
+class Computer(object):
+    def __init__(self, registers={'a': 0, 'b': 0, 'c': 0, 'd': 0}):
+        self.registers = registers
+        self.pointer = 0
 
+    def value(self, v):
+        return self.registers[v] if v in self.registers else int(v)
 
-def reg_or_value(v, regs):
-    return int(v) if v.isdigit() else regs[v]
+    def smart_add_from_reg(self, x):
+        if len(self.program) > self.pointer + 2:
+            next_instruction = self.program[self.pointer + 1]
+            if next_instruction[0] == 'dec':
+                last_instruction = self.program[self.pointer + 2]
+                if last_instruction[0] == 'jnz' and last_instruction[2] == '-2' and last_instruction[1] == next_instruction[1]:
+                    self.registers[x] += self.registers[next_instruction[1]]
+                    self.registers[next_instruction[1]] = 0
+                    self.pointer += 2
+                    return True
+        return False
 
+    def inc(self, x):
+        if not self.smart_add_from_reg(x):
+            self.registers[x] += 1
 
-def compute(registers):
-    pointer = 0
-    while pointer < len(instructions):
-        instr = instructions[pointer]
-        parts = instr.split()
-        if parts[0] == 'cpy':
-            registers[parts[2]] = reg_or_value(parts[1], registers)
-        elif parts[0] == 'inc':
-            registers[parts[1]] += 1
-        elif parts[0] == 'dec':
-            registers[parts[1]] -= 1
-        elif parts[0] == 'jnz' and reg_or_value(parts[1], registers) != 0:
-            pointer += int(parts[2])
-            continue
+    def dec(self, x):
+        self.registers[x] -= 1
 
-        pointer += 1
-    return registers
+    def cpy(self, x, y):
+        self.registers[y] = self.value(x)
+
+    def jnz(self, x, y):
+        if self.value(x):
+            self.pointer += self.value(y) - 1  # The last increment is done by the program loop
+
+    def load(self, program):
+        self.program = [tuple(instruction.strip().split()) for instruction in program]
+        return self
+
+    def execute(self, debug=False):
+        """Executes a program, which is a list of instructions"""
+        while self.pointer < len(self.program):
+            instruction = self.program[self.pointer]
+            if debug:
+                print("{:3}: {:25} {}".format(self.pointer, instruction, self.registers))
+            getattr(self, instruction[0])(*instruction[1:])
+            self.pointer += 1
+        return self
 
 
 with open('day12.in', 'r') as f:
-    instructions = [l.strip() for l in f]
+    program = f.readlines()
 
-print("Day 12.1: %s" % compute({'a': 0, 'b': 0, 'c': 0, 'd': 0})['a'])
-print("Day 12.2: %s" % compute({'a': 0, 'b': 0, 'c': 1, 'd': 0})['a'])
+print("Day 12.1: {}".format(Computer().load(program).execute().registers['a']))
+print("Day 12.2: {}".format(Computer({'a': 0, 'b': 0, 'c': 1, 'd': 0}).load(program).execute().registers['a']))
