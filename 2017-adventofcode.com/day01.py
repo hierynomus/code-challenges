4e9d92862c83645ae50fbe0719c0103d66a5cdbc
Jeroen van Erp <jeroen@hierynomus.com>
2017-12-01 11:44:36 +0100

    Shortened

--- 2017-adventofcode.com/day01.py
+++ 2017-adventofcode.com/day01.py
@@ -2,7 +2,5 @@ with open('day01.in', 'r') as f:
     s = f.readline().strip()
     out1 = sum([int(s[i]) if s[i - 1] == s[i] else 0 for i in range(0, len(s))])
     print("Day 1.1: ", out1)
-    s2 = s + s
-    steps = int(len(s) / 2)
-    out2 = sum([int(s2[i]) if s2[i] == s2[i + steps] else 0 for i in range(0, len(s))])
+    out2 = sum([int(s[i]) if s[i] == (s + s)[i + (int(len(s) / 2))] else 0 for i in range(0, len(s))])
     print("Day 2.2: ", out2)
