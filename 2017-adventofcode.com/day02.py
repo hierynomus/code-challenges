788e5a75ca889c1fe6223e2598bbf1e78e25b485
Jeroen van Erp <jeroen@hierynomus.com>
2017-12-02 11:51:15 +0100

    Day02 2017 solved

---
+++ 2017-adventofcode.com/day02.py
@@ -0,0 +1,17 @@
+from itertools import combinations
+
+rows = []
+with open('day02.in', 'r') as f:
+    for l in f.readlines():
+        rows.append(sorted([int(i) for i in l.strip().split()], reverse=True))
+
+checksum = 0
+evenly_sum = 0
+for row in rows:
+    checksum += row[0] - row[-1]
+    for c in combinations(row, 2):
+        if (c[0] % c[1]) == 0:
+            evenly_sum += int(c[0] / c[1])
+
+print("Day 2.1: ", checksum)
+print("Day 2.2: ", evenly_sum)
