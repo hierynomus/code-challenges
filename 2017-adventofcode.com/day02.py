94dec996bcb60f032dec592d0cb777c5d9a6c7c6
Jeroen van Erp <jeroen@hierynomus.com>
2017-12-02 11:54:59 +0100

    shortened

--- 2017-adventofcode.com/day02.py
+++ 2017-adventofcode.com/day02.py
@@ -9,9 +9,7 @@ checksum = 0
 evenly_sum = 0
 for row in rows:
     checksum += row[0] - row[-1]
-    for c in combinations(row, 2):
-        if (c[0] % c[1]) == 0:
-            evenly_sum += int(c[0] / c[1])
+    evenly_sum += [int(c[0] / c[1]) for c in combinations(row, 2) if c[0] % c[1] == 0][0]
 
 print("Day 2.1: ", checksum)
 print("Day 2.2: ", evenly_sum)
