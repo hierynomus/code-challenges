995621e4f59749e28120b38aa118d86484634d46
Jeroen van Erp <jeroen@hierynomus.com>
2017-12-06 09:27:14 +0100

    Day 6 solved

---
+++ 2017-adventofcode.com/day06.py
@@ -0,0 +1,24 @@
+with open('day06.in', 'r') as f:
+    inp = list(map(int, f.readline().split()))
+
+seen = []
+mem = inp[:]
+count = 0
+l = len(mem)
+r = range(l)
+print(0, inp)
+while mem not in seen:
+    seen.append(mem[:])
+    idx = max(r, key=mem.__getitem__)
+    v = mem[idx]
+    mem[idx] = 0
+    d, m = v // l, v % l
+    idx += 1
+    for i in r:
+        mem[(idx + i) % l] += d + (1 if m > 0 else 0)
+        m -= 1
+    count += 1
+    # print(count, mem)
+
+print("Day 6.1: ", count)
+print("Day 6.2: ", count - seen.index(mem))
