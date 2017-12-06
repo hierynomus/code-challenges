d17106316a2769820cfd38d0131e1ed0867a5e65
Jeroen van Erp <jeroen@hierynomus.com>
2017-12-06 09:33:27 +0100

    Speedup

--- 2017-adventofcode.com/day06.py
+++ 2017-adventofcode.com/day06.py
@@ -1,13 +1,14 @@
 with open('day06.in', 'r') as f:
     inp = list(map(int, f.readline().split()))
 
-seen = []
+seen = {}
 mem = inp[:]
 count = 0
 l = len(mem)
 r = range(l)
-while mem not in seen:
-    seen.append(mem[:])
+s = ",".join(map(str, mem))
+while s not in seen:
+    seen[s] = count
     idx = max(r, key=mem.__getitem__)
     v = mem[idx]
     mem[idx] = 0
@@ -17,6 +18,7 @@ while mem not in seen:
         mem[(idx + i) % l] += d + (1 if m > 0 else 0)
         m -= 1
     count += 1
+    s = ",".join(map(str, mem))
 
 print("Day 6.1: ", count)
-print("Day 6.2: ", count - seen.index(mem))
+print("Day 6.2: ", count - seen[s])
