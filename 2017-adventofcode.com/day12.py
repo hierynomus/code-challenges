a73e3a731d7e3610e73dcdf93ae0aa5b18fb9cf0
Jeroen van Erp <jeroen@hierynomus.com>
2017-12-12 11:11:26 +0100

    Removed reverse edge

--- 2017-adventofcode.com/day12.py
+++ 2017-adventofcode.com/day12.py
@@ -7,7 +7,6 @@ with open('day12.in', 'r') as f:
         s, ds = l.strip().split(' <-> ')
         for d in ds.split(', '):
             G.add_edge(int(s), int(d))
-            G.add_edge(int(d), int(s))
 
 print("Day 12.1:", len(nx.node_connected_component(G, 0)))
 print("Day 12.2:", len(list(nx.connected_components(G))))
