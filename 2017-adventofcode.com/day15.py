755b87e114e79c1d53add67ef75ac95d67600fe2
Jeroen van Erp <jeroen@hierynomus.com>
2017-12-15 14:51:21 +0100

    Optimized the generator function

--- 2017-adventofcode.com/day15.py
+++ 2017-adventofcode.com/day15.py
@@ -2,11 +2,24 @@ inp_a, inp_b = 679, 771
 modulus = 2147483647
 
 
-def nr_generator(inp, mult, modulus, divisor):
-    x = inp
+def nr_generator(seed, mult, modulus, divisor):
+    x = seed
     while True:
         x = (x * mult) % modulus
-        if x % divisor == 0:
+        if x & (divisor - 1) == 0:
+            yield x
+
+
+def nr_generator_opt(x, a, m, divisor):
+    """https://programmingpraxis.com/2014/01/14/minimum-standard-random-number-generator/"""
+    while True:
+        t = a * x
+        p = t >> 31
+        q = t & 0x7FFFFFFF
+        x = p + q
+        if x > m:
+            x -= m
+        if x & (divisor - 1) == 0:
             yield x
 
 
@@ -20,5 +33,6 @@ def count_same(gen_a, gen_b, rounds):
     return counter
 
 
-print("Day 15.1:", count_same(nr_generator(inp_a, 16807, modulus, 1), nr_generator(inp_b, 48271, modulus, 1), 40000000))
-print("Day 15.2:", count_same(nr_generator(inp_a, 16807, modulus, 4), nr_generator(inp_b, 48271, modulus, 8), 5000000))
+# assert count_same(nr_generator(inp_a, 16807, modulus, 1), nr_generator_opt(inp_a, 16807, modulus, 1), 10000) == 10000
+print("Day 15.1:", count_same(nr_generator_opt(inp_a, 16807, modulus, 1), nr_generator_opt(inp_b, 48271, modulus, 1), 40000000))
+print("Day 15.2:", count_same(nr_generator_opt(inp_a, 16807, modulus, 4), nr_generator_opt(inp_b, 48271, modulus, 8), 5000000))
