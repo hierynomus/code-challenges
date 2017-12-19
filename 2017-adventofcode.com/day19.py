f0b85ffd107fefb78b0ad0714e8da1d5fba9a777
Jeroen van Erp <jeroen@hierynomus.com>
2017-12-19 10:28:35 +0100

    Removed prints

--- 2017-adventofcode.com/day19.py
+++ 2017-adventofcode.com/day19.py
@@ -21,20 +21,16 @@ while True:
     count += 1
     c = grid[n_y][n_x]
     if c == '+' and d in ['n', 's']:
-        print(d, n_x, n_y)
         d = 'e' if n_x + 1 < len(grid[n_y]) and grid[n_y][n_x + 1] == '-' else 'w'
     elif c == '+' and d in ['e', 'w']:
-        print(d, n_x, n_y)
         d = 's' if n_y + 1 < len(grid) and grid[n_y + 1][n_x] == '|' else 'n'
     elif c == '|' or c == '-':
         pass
     elif c != ' ':
         seen.append(c)
-        print(seen)
     else:
         break
     x, y = n_x, n_y
 
 print("Day 19.1:", ''.join(seen))
 print("Day 19.2:", count)
-
