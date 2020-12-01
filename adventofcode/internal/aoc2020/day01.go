2c2e0ee497a31acfa953d398bf205d65cb4e5030
Jeroen van Erp <jeroen@hierynomus.com>
2020-12-01 16:19:17 +0100

    Simplified day01

--- adventofcode/internal/aoc2020/day01.go
+++ adventofcode/internal/aoc2020/day01.go
@@ -8,25 +8,37 @@ import (
 )
 
 func Day01(reader *bufio.Scanner) (string, string) {
-	expense := []int{}
+	var part1, part2 int
+
+	expense := aoc.IntSet{}
+
 	for reader.Scan() {
-		expense = append(expense, aoc.ToInt(reader.Text()))
-	}
-
-	var part1 int
-
-	for c := range aoc.IntCombinationsN(expense, 2) {
-		if aoc.Sum(c) == 2020 {
-			part1 = c[0] * c[1]
+		i := aoc.ToInt(reader.Text())
+		if expense.Contains(2020 - i) {
+			part1 = i * (2020 - i)
 		}
+
+		for j := range expense {
+			if expense.Contains(2020 - i - j) {
+				part2 = i * j * (2020 - i - j)
+			}
+		}
+
+		expense.Add(i)
 	}
 
-	part2 := 0
-	for c := range aoc.IntCombinationsN(expense, 3) {
-		if aoc.Sum(c) == 2020 {
-			part2 = c[0] * c[1] * c[2]
-		}
-	}
+	// for c := range aoc.IntCombinationsN(expense, 2) {
+	// 	if aoc.Sum(c) == 2020 {
+	// 		part1 = c[0] * c[1]
+	// 	}
+	// }
+
+	// part2 := 0
+	// for c := range aoc.IntCombinationsN(expense, 3) {
+	// 	if aoc.Sum(c) == 2020 {
+	// 		part2 = c[0] * c[1] * c[2]
+	// 	}
+	// }
 
 	return strconv.Itoa(part1), strconv.Itoa(part2)
 }
