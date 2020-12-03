0bd39a0a11d5b9a95692a89f27313b916568a2de
Jeroen van Erp <jeroen@hierynomus.com>
2020-12-03 10:33:25 +0100

    Simplify setup a bit

---
+++ adventofcode/internal/aoc2018/aoc2018.go
@@ -0,0 +1,15 @@
+package aoc2018
+
+import "github.com/hierynomus/code-challenges/adventofcode/pkg/day"
+
+var AllDays map[int]day.Solver = map[int]day.Solver{
+	1: Day01,
+	2: Day02,
+	3: Day03,
+	4: Day04,
+	5: Day05,
+	6: Day06,
+	7: Day07,
+	8: Day08,
+	9: Day09,
+}
---
+++ adventofcode/internal/aoc2020/aoc2020.go
@@ -0,0 +1,9 @@
+package aoc2020
+
+import "github.com/hierynomus/code-challenges/adventofcode/pkg/day"
+
+var AllDays map[int]day.Solver = map[int]day.Solver{
+	1: Day01,
+	2: Day02,
+	3: Day03,
+}
